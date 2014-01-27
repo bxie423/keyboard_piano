11e9f0c76f07fc2da2328007dfb53cd4b2a0dbb6
Bernard Xie <bxie423@gmail.com>
2014-01-27 11:39:28 -0600

    updated script

--- keyboard_piano.sh
+++ keyboard_piano.sh
@@ -22,7 +22,8 @@ The keys z through , correspond to C3 through C4.
 In order to play a sharp, simply hold down SHIFT while pressing a note.
 In order to exit, press the '\`' (backtick) key.
 
-NOTE:  If this program exits incorrectly, you might need to type 'stty echo' in order to be able to see what you are typing again."
+NOTE:  If this program exits incorrectly, you might need to type 'stty echo' in order to be able to see what you are typing again.
+"
 
 # Turn off screen echo
 stty -echo
@@ -38,14 +39,15 @@ function play_note {
 # An associative array of keypress-note pairs
 declare -A KEYMAP=( ["q"]=C5 ["Q"]=Db5 ["w"]=D5 ["W"]=Eb5 ["e"]=E5 ["r"]=F5 ["R"]=Gb5 ["t"]=G5 ["T"]=Ab5 ["y"]=A5 ["Y"]=Bb5 ["u"]=B5 ["i"]=C6 ["a"]=C4 ["A"]=Db4 ["s"]=D4 ["S"]=Eb4 ["d"]=E4 ["f"]=F4 ["F"]=Gb4 ["g"]=G4 ["G"]=Ab4 ["h"]=A4 ["H"]=Bb4 ["j"]=B4 ["k"]=C5 ["z"]=C3 ["Z"]=Db3 ["x"]=D3 ["X"]=Eb3 ["c"]=E3 ["v"]=F3 ["V"]=Gb3 ["b"]=G3 ["B"]=Ab3 ["n"]=A3 ["N"]=Bb3 ["m"]=B3 [","]=C4 )
 
+# Main execution begins here
+
 # Hopefully this loads every WAV file into memory, eliminating the delay
 # that users would experience when they press a note for the first time
-for note in "${KEYMAP[@]}"
-do
-    mplayer -volume 0 ${dir}/${note}.wav > /dev/null 2>&1 &
-done
 
-# Most of the magic happens here
+echo "Initializing note files..."
+cat ${dir}/* > /dev/null
+echo "Done!"
+
 while [ "$PRESS" != "\`" ]
 do
     read -n 1 PRESS
